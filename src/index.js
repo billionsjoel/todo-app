9dc89b6ebc591c98efe925c9df3a84cf6d82981c
Billions Joel <joelatugonza@gmail.com>
2022-01-12 13:26:55 +0300

    Add refresh animation

--- src/index.js
+++ src/index.js
@@ -4,16 +4,26 @@ import renderTask from './modules/RenderTask.js';
 import addTask from './modules/AddTask.js';
 import renderAllTasks from './modules/RenderAllTasks.js';
 
+const refresh = document.querySelector('.refresh');
 const pushTaskToDom = document.querySelector('.add-task');
 const allTasks = [];
 
 allTasks.sort(compare).map((task) => {
-  renderTask(task);
-  return task;
+	renderTask(task);
+	return task;
 });
 
 renderAllTasks();
 
 if (pushTaskToDom) {
-  pushTaskToDom.addEventListener('click', addTask);
+	pushTaskToDom.addEventListener('click', addTask);
+}
+
+function spin() {
+	refresh.classList.add('rotate');
+	setTimeout(() => refresh.classList.remove('rotate'), 1000);
+}
+
+if (refresh) {
+	refresh.addEventListener('click', spin);
 }
