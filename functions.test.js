a3294a5267e301d1a5fbcbe8fbdedb33b7593631
Billions Joel <joelatugonza@gmail.com>
2022-01-19 07:40:12 +0300

    Add tests for add method

--- functions.test.js
+++ functions.test.js
@@ -2,12 +2,14 @@ import Tasks from './src/modules/TasksClass';
 
 const tasks = new Tasks();
 
-test('first element added', () => {
-	tasks.addTask({ description: 'First task' });
-	expect(tasks.allTasks.length).toBe(1);
-});
+describe('Testing add method ', () => {
+	test('first element added', () => {
+		tasks.addTask({ description: 'First task' });
+		expect(tasks.allTasks.length).toBe(1);
+	});
 
-test('task description passes', () => {
-	tasks.addTask({ description: 'New Task' });
-	expect(tasks.allTasks[1].description).toBe('New Task');
+	test('task description passes', () => {
+		tasks.addTask({ description: 'New Task' });
+		expect(tasks.allTasks[1].description).toBe('New Task');
+	});
 });
