c18ce3d21e959fffbd987b0e7ff17361c15a0372
Billions Joel <joelatugonza@gmail.com>
2022-01-19 07:17:37 +0300

    Fix eslint errors

--- src/modules/TasksClass.js
+++ src/modules/TasksClass.js
@@ -1,38 +1,38 @@
 export default class Tasks {
-	constructor(storage) {
-		this.allTasks = storage ? JSON.parse(storage) : [];
-	}
+  constructor(storage) {
+    this.allTasks = storage ? JSON.parse(storage) : [];
+  }
 
-	addTask(task) {
-		task.index = this.allTasks.length + 1;
-		task.completed = false;
-		this.allTasks.push(task);
-	}
+  addTask(task) {
+    task.index = this.allTasks.length + 1;
+    task.completed = false;
+    this.allTasks.push(task);
+  }
 
-	removeTask(index) {
-		this.allTasks = this.allTasks.filter((task) => task.index !== index);
-		this.allTasks = this.allTasks.map((task) => {
-			if (task.index > index) {
-				task.index -= 1;
-			}
-			return task;
-		});
-	}
+  removeTask(index) {
+    this.allTasks = this.allTasks.filter((task) => task.index !== index);
+    this.allTasks = this.allTasks.map((task) => {
+      if (task.index > index) {
+        task.index -= 1;
+      }
+      return task;
+    });
+  }
 
-	editTask(task) {
-		this.allTasks[task.index - 1] = task;
-	}
+  editTask(task) {
+    this.allTasks[task.index - 1] = task;
+  }
 
-	sort(oldIndex, newIndex) {
-		this.allTasks[oldIndex - 1].index = newIndex;
-	}
+  sort(oldIndex, newIndex) {
+    this.allTasks[oldIndex - 1].index = newIndex;
+  }
 
-	clearMarkedTasks() {
-		this.allTasks = this.allTasks.sort((a, b) => a.index - b.index);
-		this.allTasks = this.allTasks.filter((task) => !task.completed);
-		this.allTasks = this.allTasks.map((task, i) => {
-			task.index = i + 1;
-			return task;
-		});
-	}
+  clearMarkedTasks() {
+    this.allTasks = this.allTasks.sort((a, b) => a.index - b.index);
+    this.allTasks = this.allTasks.filter((task) => !task.completed);
+    this.allTasks = this.allTasks.map((task, i) => {
+      task.index = i + 1;
+      return task;
+    });
+  }
 }
